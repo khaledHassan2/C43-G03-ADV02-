6dbf927b33fcb3abb368052dac6bd662a3cbbaf1
khaledHassan2 <khaledamalb22@gmail.com>
2025-01-17 18:31:13 +0200

    v01)non genaric collection [array list]

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region non genaric collection [array list]
+            //ArrayList array = new ArrayList();
+            //Console.WriteLine($"count = {array.Count}  / capacity: {array.Capacity}");
+
+            //array.Add( 1 );
+            //array.Add( 2 );
+            //array.AddRange( new object[] { 3,4 } );
+            //Console.WriteLine("after add elements");
+            //Console.WriteLine($"count = {array.Count}  / capacity: {array.Capacity}");
+
+            //array.Add( 5 );
+            //Console.WriteLine("after add element 5");
+            //Console.WriteLine($"count = {array.Count}  / capacity: {array.Capacity}");
+
+            //array.TrimToSize();
+            //Console.WriteLine("after triming");
+            //Console.WriteLine($"count = {array.Count}  / capacity: {array.Capacity}");
+
+
+
+            #endregion
         }
     }
 }
